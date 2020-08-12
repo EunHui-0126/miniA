9435ec6263035bfbb034aa55fefb0939d4fcb976
hwanhee <poiu419@naver.com>
2020-08-12 14:33:00 +0900

    화니

--- mini/views.py
+++ mini/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.http import JsonResponse # JSON 응답
+from app.models import phone
+from django.forms.models import model_to_dict
+
 
 def main(request):
     return render(request,'base.html')
+
+def phone_data(request):
+    data = phone.objects.all()
+    return render(request,'base.html',{'data':data})
+
+# def phone_data(request):
+#     data = phone.objects.all()
+#     phone_list = []
+#     for p in data:
+#         d = model_to_dict(p) # QuerySet -> Dict
+#         phone_list.append(d)
+#     return JsonResponse(phone_list, safe=False)
