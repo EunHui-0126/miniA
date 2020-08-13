69d4fd7fa1252bec51701740efb5a2f27cc72ef5
hwanhee <poiu419@naver.com>
2020-08-13 13:13:28 +0900

    ㅇ

--- mini/views.py
+++ mini/views.py
@@ -44,8 +44,10 @@ def main(request):
     soup = bs(res.text,'html.parser')
     a_list = soup.select_one('dl:nth-child(2)')
     data = phone.objects.all()
+    i=random.randint(1,43)
+    r=menu.objects.get(id=i)
+    return render(request,'index.html',{'a_list':a_list.get_text('"\n"'),'data':data,'r':r})
 
-    return render(request,'index.html',{'a_list':a_list.get_text('"\n"'),'data':data})
 
 
 def phone_data(request):
@@ -54,6 +56,3 @@ def phone_data(request):
     
 
 
-    i=random.randint(1,43)
-    r=menu.objects.get(id=i)
-    return render(request,'index.html',{'a_list':a_list.get_text('"\n"'),'data':data,'r':r})
