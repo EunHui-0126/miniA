5ed0c6d1b9f6585f477ecf875757db34183e0736
PARK <parkch7763@gmail.com>
2020-08-13 16:28:55 +0900

    a

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class menu(models.Model):
     restaurant = models.CharField(max_length=50)
     food = models.CharField(max_length=50)
-
+    photo = models.CharField(max_length=1000)
 
 class Article(models.Model):
     # id = models.CharField(max_length=100)
